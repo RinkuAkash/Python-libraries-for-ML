b5e801d2c33060a0f416554bdb30b6ab2c3efb13
RinkuAkash <akashrinku2@outlook.com>
2020-01-21 14:36:20 +0530

    Added: program to create one dimensional array using pandas

---
+++ Pandas/01_create_1d_array.py
@@ -0,0 +1,13 @@
+'''
+Created on 21/01/2020
+@author: B Akash
+'''
+'''
+Problem statement:
+Write a Python program to create and display a one-dimensional array-like object containing an array of data using Pandas module.
+'''
+
+import pandas as pd 
+
+array=pd.Series([1,2,3,4,5])
+print(array)
