67762e4083826968d4d6345598c20c80de0d1e02
RinkuAkash <akashrinku2@outlook.com>
2020-01-21 15:10:34 +0530

    Added: Program to display specific columns of data frames

---
+++ Pandas/08_display_specific_columns.py
@@ -0,0 +1,21 @@
+'''
+Created on 21/01/2020
+@author: B Akash
+'''
+'''
+Problem statement:
+Write a Python program to select the 'name' and 'score' columns from the following DataFrame. 
+Sample Python dictionary data and list labels:
+'''
+
+import pandas as pd 
+import numpy as np 
+
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+dataFrame= pd.DataFrame(exam_data,labels)
+print(dataFrame[['name','score']])
